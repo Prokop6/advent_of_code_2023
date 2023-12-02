cc20891ce85d7d7cbd1b071bf1133c6968bd991b
Marcin <mr.prokop.6+dev@gmail.com>
2023-12-02 19:17:38 +0100

    day 2 part 2

--- advent_of_code_2023/tests/test_day_2/test_integration.py
+++ advent_of_code_2023/tests/test_day_2/test_integration.py
@@ -1,1 +1,22 @@
 from advent_of_code_2023.src.day_2 import part_1, part_2
+
+import os
+
+
+def get_data(file):
+  data: str
+
+  with open(os.path.join(os.getcwd(), "inputs", file), "r") as file:
+    data = file.read()
+
+  return data
+
+
+def test_part_1_solution():
+
+  assert (part_1.app(get_data("sample_2a.txt")) == 8)
+
+
+def test_part_2_solution():
+
+  assert (part_2.app(get_data("sample_2a.txt")) == 2286)
--- advent_of_code_2023/tests/test_day_2/test_utils.py
+++ advent_of_code_2023/tests/test_day_2/test_utils.py
@@ -36,3 +36,16 @@ def test_getting_game_number(sample, expected):
 ])
 def test_getting_rgb_data(sample, expected):
   assert (expected == utils.get_rgb_values(sample))
+
+
+@pytest.mark.parametrize("sample,expected", [
+    ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", (4, 2, 6)),
+    ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", (1, 3, 4)),
+    ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", (20, 13, 6)),
+    ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", (14, 3, 15)),
+    ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", (6, 3, 2))
+])
+def test_get_minimal_set(sample, expected):
+  new_game = utils.parse_record(sample)
+
+  assert (expected == new_game.get_minimal_set())
